949539432fce1c39da929e96c6bb66fb3a895458
RockStone <RockStoneStudios666@gmail.com>
2022-03-26 19:05:27 -0500

    Jummm

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const UsuarioRegistrar = require('./Routes/usuario.route');
 
 
 //Settings 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
 
-app.use(cors());
+app.use(cors(corsOptions));
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
